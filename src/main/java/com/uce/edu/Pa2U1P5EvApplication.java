c2e9d392c623a22a1c9e8b9ee0aa6031ecf0e562
Emil2245 <emilmontiel@hotmail.com>
2023-11-07 18:54:46 -0500

    mi primer commit, subida del proyecto

---
+++ src/main/java/com/uce/edu/Pa2U1P5EvApplication.java
@@ -0,0 +1,26 @@
+package com.uce.edu;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5EvApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5EvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("Soy Emil Verkade");
+		LocalDateTime now = LocalDateTime.now();
+        System.out.println("Hora actual: " + now);
+	}
+
+	
+}
